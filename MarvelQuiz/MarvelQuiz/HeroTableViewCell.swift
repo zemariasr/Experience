02a9875f656b944058e619a04808cdf1e472c994
José Maria <zemariasr@me.com>
2018-11-05 16:52:59 -0200

    Configurado algumas constrains, adicionado figuras e cores. Adicionado arquivos de Controles das Views.

---
+++ MarvelQuiz/MarvelQuiz/HeroTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HeroTableViewCell.swift
+//  MarvelQuiz
+//
+//  Created by José Maria Só Rodrigues on 05/11/2018.
+//  Copyright © 2018 ZM FIGHTER SYSTEM. All rights reserved.
+//
+
+import UIKit
+
+class HeroTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
