2a4a498f481547c04b6f6003bce3f747d7fc0cac
José Maria <zemariasr@me.com>
2018-11-05 15:20:14 -0200

    Planejamento das telas pronto.

---
+++ MarvelQuiz/MarvelQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarvelQuiz
+//
+//  Created by José Maria Só Rodrigues on 05/11/2018.
+//  Copyright © 2018 ZM FIGHTER SYSTEM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
