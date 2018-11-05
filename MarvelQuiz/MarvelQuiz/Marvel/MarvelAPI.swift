52f7994153826c7a15fd3b0fe798f5fa275b75c1
José Maria <zemariasr@me.com>
2018-11-05 17:55:58 -0200

    Iniciando a parte de estrutura conforme o API da marvel.

---
+++ MarvelQuiz/MarvelQuiz/Marvel/MarvelAPI.swift
@@ -0,0 +1,9 @@
+//
+//  MarvelAPI.swift
+//  MarvelQuiz
+//
+//  Created by José Maria Só Rodrigues on 05/11/2018.
+//  Copyright © 2018 ZM FIGHTER SYSTEM. All rights reserved.
+//
+
+import Foundation
---
+++ MarvelQuiz/MarvelQuiz/Marvel/MarvelClasses.swift
@@ -0,0 +1,53 @@
+//
+//  MarvelClasses.swift
+//  MarvelQuiz
+//
+//  Created by José Maria Só Rodrigues on 05/11/2018.
+//  Copyright © 2018 ZM FIGHTER SYSTEM. All rights reserved.
+//
+
+import Foundation
+
+struct MarvelInfo: Codable {
+    let code: Int
+    let status: String
+    let data: MarvelData
+    
+}
+
+struct MarvelData: Codable {
+    let offset: Int
+    let limit: Int
+    let total: Int
+    let count: Int
+    let results: [Hero]
+}
+
+struct Hero: Codable {
+    let id: Int
+    let name: String
+    let description: String
+    let thumbnail: Thumbnail
+    let urls: [HeroURL]
+}
+
+struct Thumbnail: Codable {
+    let path: String
+    let ext: String
+    
+    var url: String {
+        return path + "." + ext
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case path
+        case ext = "extension"
+    }
+    
+}
+
+struct HeroURL: Codable {
+    let type: String
+    let url: String
+    
+}
